55c6f9e787c0a53a2043a4c33e7faa35d8d6f67b
BobJohn69 <minhtuan030304@gmail.com>
2025-06-04 22:01:55 +0700

    Chỉnh sửa trang cá nhân

--- src/pages/account/EditProfile.jsx
+++ src/pages/account/EditProfile.jsx
@@ -190,7 +190,7 @@ const EditProfile = () => {
             />
           </div>
 
-          <div>
+          {/* <div>
             <label className="block text-sm font-medium text-gray-700">
               Date of Birth
             </label>
@@ -200,7 +200,7 @@ const EditProfile = () => {
               onChange={(e) => setDateOfBirth(e.target.value)}
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
             />
-          </div>
+          </div> */}
         </div>
 
         {/* Action Buttons */}
