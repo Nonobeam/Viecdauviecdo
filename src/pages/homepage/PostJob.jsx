ced433803c81abef5a5507f94c12c078b800718c
Hyudeque <quanghuy01062004@gmail.com>
2025-06-05 21:08:40 +0700

    Fix

--- src/pages/homepage/PostJob.jsx
+++ src/pages/homepage/PostJob.jsx
@@ -6,7 +6,6 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Checkbox } from "@/components/ui/checkbox"
-import { RadioGroup, RadioGroupItem } from "@/components/radiogroup"
 import { Label } from "@/components/ui/label"
 import {
   ArrowLeft,
