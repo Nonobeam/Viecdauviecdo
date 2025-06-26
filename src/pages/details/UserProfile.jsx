f4772209bb099ea3bddeda911ab59a8aec8353b2
BobJohn69 <minhtuan030304@gmail.com>
2025-06-26 18:35:11 +0700

    Unchange path to edit profile

--- src/pages/details/UserProfile.jsx
+++ src/pages/details/UserProfile.jsx
@@ -408,7 +408,7 @@ const Profile = () => {
                         variant="outline"
                         className="w-full justify-start border-purple-200 text-purple-700 hover:bg-purple-50"
                         onClick={() =>
-                          navigate("/edit-profile", {
+                          navigate("/change-profile", {
                             state: { userInformation },
                           })
                         }
